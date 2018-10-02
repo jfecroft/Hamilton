8ff8e501b5628b11555bc69f4785eb8e0fe0ffb1
jfecroft <j.f.e.croft@gmail.com>
2018-10-02 16:20:42 +1300

    WIP

--- src/Ham.py
+++ src/Ham.py
@@ -3,52 +3,19 @@ from itertools import groupby, combinations, product
 from scipy.misc import comb
 from scipy.integrate import solve_ivp
 from yaml import load
+from generic import reduce_output
 
 dim = 2 # spatial dimension
 num = 3 # particle number
 n = dim*num
 pairs = int(comb(num, 2, exact=True))
 
-def reduce_output(func, item, *args, **kwargs):
-    """
-    simple function to reduce output from existing functions
-
-    if func returns an iterable - just return item
-    """
-    def inner_func(*args, **kwargs):
-        return func(*args, **kwargs)[item]
-    return inner_func
 
 def lj(r2, C12, C6):
     """
     lennard jones potentail r2 is r**2
     """
     return C12/r2**6 - C6/r2**3
-
-def is_q(var):
-    """
-    test if a variable is a q
-    """
-    return var.name[0] == 'q'
-
-def is_p(var):
-    """
-    test if a variable is a p
-    """
-    return var.name[0] == 'p'
-
-def get_dim(var):
-    """
-    return the dimension of var
-    """
-    return int(var.name.split('_')[1])
-
-def get_num(var):
-    """
-    return the paritcle number of var
-    """
-    return int(var.name.split('_')[2])
-
 
 def T(coords, system_def, system_vars):
     """
@@ -73,9 +40,6 @@ def V(coords, system_def, system_vars):
         pot += lj(r2,c12,c6)
     return pot
 
-def get_coords(coords):
-    return [i for i in coords.itervalues()]
-
 def load_yaml(filen):
     """
     load a yaml file and return the json object
@@ -84,14 +48,47 @@ def load_yaml(filen):
         return_dict = load(open_file)
     return return_dict
 
-def create_coords(dim, num):
-    coords = []
-    for i, j, k in product(dim, range(num), ('p', 'q')):
-        coords.append(sp.var('{}_{}_{}'.format(k,i,j)))
-    return tuple(coords)
 
+class Hamilton:
+    def __init__(self, T, V, input_file):
+        self.data = load_yaml('input')
+        self.system_def = self.data['system_def']
+        self.dim = self.system_def['dim']
+        self.num = self.system_def['num']
+        self.system_vars = self.data['system_vars']
+        self.coords = self.create_coords(self.dim, self.num)
+        self.coord_dict = {i.name:i for i in self.coords}
+        self.T = T(self.coord_dict, self.system_def, self.system_vars)
+        self.V = V(self.coord_dict, self.system_def, self.system_vars)
+        self.H = self.T + self.V
+        self.ps = [i for i in self.coords if i.name[0] == 'p']
+        self.qs = [i for i in self.coords if i.name[0] == 'q']
+        import pdb
+        pdb.set_trace()
+
+    def create_coords(self, dim, num):
+        coords = []
+        for i, j, k in product(dim, range(num), ('p', 'q')):
+            coords.append(sp.var('{}_{}_{}'.format(k,i,j)))
+        return tuple(coords)
+
+    def prop(self, time):
+        HJacp = sp.Matrix([self.H]).jacobian(self.ps)
+        HJacq = -sp.Matrix([self.H]).jacobian(self.qs)
+        RHS = sp.Matrix(sp.BlockMatrix([[HJacp, HJacq]]))
+        t = sp.var('t')
+        import pdb
+        pdb.set_trace()
+        func = reduce_output(sp.lambdify((t,(self.ps+self.qs)), RHS), 0)
+        # func is defind as ps+qs therefore we must pass qs + ps - see hamilton equations
+        solve_ivp(func, (0,100), range(12))
+
+
+
+
+Ham = Hamilton(T, V, 'input')
+Ham.prop(10)
 # define canonical ps and qs
-t = sp.var('t')
 data = load_yaml('input')
 system_vars = data['system_vars']
 system_def = data['system_def']
@@ -108,12 +105,4 @@ print system_vars
 KE = T(coord_dict,system_def, system_vars)
 POT = V(coord_dict, system_def, system_vars)
 H = KE+POT
-HJacp = sp.Matrix([H]).jacobian(ps)
-HJacq = -sp.Matrix([H]).jacobian(qs)
-RHS = sp.Matrix(sp.BlockMatrix([[HJacp, HJacq]]))
-import pdb
-pdb.set_trace()
-func = reduce_output(sp.lambdify((t,(ps+qs)), RHS), 0)
-
-solve_ivp(func, (0,100), range(12))
 
