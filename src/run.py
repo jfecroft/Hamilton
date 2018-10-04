9dfa9fed7840b3524aaf50a5163e7d8163a16ee3
jfecroft <j.f.e.croft@gmail.com>
2018-10-04 15:50:14 +1300

    WIP

--- src/run.py
+++ src/run.py
@@ -6,10 +6,15 @@ from scipy.constants import physical_constants
 au2wavenumbers = physical_constants["hartree-inverse meter relationship"][0]/100.0
 kelvin2hartree = physical_constants["kelvin-hartree relationship"][0]
 
-def gen_initial_condition(theta, nrg, R, r, **kwargs):
+def gen_initial_condition(theta, nrg, R, r, mass_0, mass_1, mass_2, **kwargs):
+    """
+    generate initial conditions for atom+dimer run
+    not quite right want equal and opposite momentum not velocity
+    """
     ic = {}
     dy = r*sin(theta)
     dx = r*cos(theta)
+
     ic['q_x_0'] = -R/2.0
     ic['q_y_0'] = 0
 
@@ -19,24 +24,26 @@ def gen_initial_condition(theta, nrg, R, r, **kwargs):
     ic['q_x_2'] = R/2.0 - dx
     ic['q_y_2'] = -dy
 
-    p = sqrt(nrg*2.0*system_vars['mass_0']/3.0)
-    ic['p_x_0'] = p
-    ic['p_x_1'] = -p
-    ic['p_x_2'] = -p
+    total_mass = mass_0 + mass_1 + mass_2
+    p0 = sqrt(2.0*nrg*mass_0**2/total_mass)
+    p1 = sqrt(2.0*nrg*mass_1**2/total_mass)
+    p2 = sqrt(2.0*nrg*mass_2**2/total_mass)
+    ic['p_x_0'] = p0
+    ic['p_x_1'] = -p1
+    ic['p_x_2'] = -p2
     return ic
 
 data = load_yaml('input')
 num = data['system_def']['num']
 dim = data['system_def']['dim']
-initial_condition = data['initial_condition']
+# initial_condition = data['initial_condition']
 system_vars = data['system_vars']
 Ham = Hamilton(num, dim, T, V, **system_vars)
 r = 8.77967500274974/2
 R = 200
 nrg = 10.0*kelvin2hartree
-theta = pi/2
+theta = pi/1.345
 ic = gen_initial_condition(theta, nrg, R, r, **system_vars)
-initial_condition.update(ic)
-pprint(initial_condition)
-sol = Ham.prop(10000000, initial_condition)
+pprint(ic)
+sol = Ham.prop(10000000, ic)
 pprint(sol)
